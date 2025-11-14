76333a827b8a21a9dc47f63cdd14841ad18621da
Sebastian Ullrich <sebasti@nullri.ch>
2025-11-14 16:22:22 +0100

    keep

--- Mathlib/Tactic/Attr/Core.lean
+++ Mathlib/Tactic/Attr/Core.lean
@@ -3,7 +3,7 @@ Copyright (c) 2023 Yury Kudryashov. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yury Kudryashov
 -/
-module
+module  -- shake: keep-downstream (out-of-line `attribute`)
 
 public meta import Mathlib.Tactic.Attr.Register
 
