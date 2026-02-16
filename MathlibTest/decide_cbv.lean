ec289ff5a10ca205d75079b3f087acaa548bf10a
Wojciech Rozowski <wojciech@lean-fro.org>
2026-02-16 14:12:35 +0000

    chore: add `IsPrimePow` tests

--- MathlibTest/decide_cbv.lean
+++ MathlibTest/decide_cbv.lean
@@ -1,5 +1,9 @@
 import Mathlib.Algebra.IsPrimePow
 
+example : IsPrimePow 10093 := by decide_cbv
+
+example : IsPrimePow 100999 := by decide_cbv
+
 example : ¬ IsPrimePow 15 := by decide_cbv
 
 example : ¬ IsPrimePow 111111111111111155 := by decide_cbv
