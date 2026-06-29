414ff70e95c7041e2db3b2478d93711abc938a4e
Yaël Dillies <yael.dillies@gmail.com>
2026-06-29 08:10:22 +0200

    chore: adaptations for batteries-pr-testing-1853

--- Mathlib/Data/List/ChainOfFn.lean
+++ Mathlib/Data/List/ChainOfFn.lean
@@ -11,4 +11,19 @@ public import Mathlib.Tactic.Finiteness.Attr
 public import Mathlib.Tactic.ToDual
 public import Mathlib.Util.CompileInductive
 
-deprecated_module (since := "2026-06-16")
+/-!
+# Lemmas about `IsChain` and `ofFn`
+This file provides lemmas involving both `List.IsChain` and `List.ofFn`.
+-/
+
+public section
+
+open Nat
+
+namespace List
+
+lemma isChain_ofFn {α : Type*} {n : ℕ} {f : Fin n → α} {r : α → α → Prop} :
+    (ofFn f).IsChain r ↔ ∀ (i) (hi : i + 1 < n), r (f ⟨i, lt_of_succ_lt hi⟩) (f ⟨i + 1, hi⟩) := by
+  simp_rw [isChain_iff_getElem, List.getElem_ofFn, length_ofFn]
+
+end List
