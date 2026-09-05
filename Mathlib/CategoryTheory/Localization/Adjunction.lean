674b69ff8eea99e5e9e9898510000c6184ec7814
Paul Reichert <6992158+datokrat@users.noreply.github.com>
2026-09-05 16:02:50 +0000

    remove one option, addressing defeq abuse

--- Mathlib/CategoryTheory/Localization/Adjunction.lean
+++ Mathlib/CategoryTheory/Localization/Adjunction.lean
@@ -49,36 +49,28 @@ noncomputable def ε : 𝟭 D₁ ⟶ G' ⋙ F' := by
   letI : Lifting L₁ W₁ ((G ⋙ F) ⋙ L₁) (G' ⋙ F') :=
     Lifting.mk (CatCommSq.hComp G F L₁ L₂ L₁ G' F').iso.symm
   exact Localization.liftNatTrans L₁ W₁ L₁ ((G ⋙ F) ⋙ L₁) (𝟭 D₁) (G' ⋙ F')
-    (whiskerRight adj.unit L₁)
+    (L₁.leftUnitor.inv ≫ whiskerRight adj.unit L₁)
 
 set_option backward.defeqAttrib.useBackward true in
 lemma ε_app (X₁ : C₁) :
     (ε adj L₁ W₁ L₂ G' F').app (L₁.obj X₁) =
       L₁.map (adj.unit.app X₁) ≫ (CatCommSq.iso F L₂ L₁ F').hom.app (G.obj X₁) ≫
         F'.map ((CatCommSq.iso G L₁ L₂ G').hom.app X₁) := by
-  set_option backward.isDefEq.respectTransparency false in
-  let : Lifting L₁ W₁ ((G ⋙ F) ⋙ L₁) (G' ⋙ F') :=
-    Lifting.mk (CatCommSq.hComp G F L₁ L₂ L₁ G' F').iso.symm
-  simp only [ε, liftNatTrans_app, Lifting.iso, Iso.symm,
-    Functor.id_obj, Functor.comp_obj, Functor.rightUnitor_hom_app,
-      whiskerRight_app, CatCommSq.hComp_iso_hom_app, id_comp]
+  simp [ε]
 
 /-- Auxiliary definition of the counit morphism for the adjunction `Adjunction.localization` -/
 noncomputable def η : F' ⋙ G' ⟶ 𝟭 D₂ := by
   letI : Lifting L₂ W₂ ((F ⋙ G) ⋙ L₂) (F' ⋙ G') :=
     Lifting.mk (CatCommSq.hComp F G L₂ L₁ L₂ F' G').iso.symm
-  exact liftNatTrans L₂ W₂ ((F ⋙ G) ⋙ L₂) L₂ (F' ⋙ G') (𝟭 D₂) (whiskerRight adj.counit L₂)
+  exact liftNatTrans L₂ W₂ ((F ⋙ G) ⋙ L₂) L₂ (F' ⋙ G') (𝟭 D₂)
+    (whiskerRight adj.counit L₂ ≫ L₂.leftUnitor.hom)
 
 lemma η_app (X₂ : C₂) :
     (η adj L₁ L₂ W₂ G' F').app (L₂.obj X₂) =
       G'.map ((CatCommSq.iso F L₂ L₁ F').inv.app X₂) ≫
         (CatCommSq.iso G L₁ L₂ G').inv.app (F.obj X₂) ≫
         L₂.map (adj.counit.app X₂) := by
-  set_option backward.isDefEq.respectTransparency false in
-  let : Lifting L₂ W₂ ((F ⋙ G) ⋙ L₂) (F' ⋙ G') :=
-    Lifting.mk (CatCommSq.hComp F G L₂ L₁ L₂ F' G').iso.symm
-  simp only [η, liftNatTrans_app, Lifting.iso, Iso.symm, CatCommSq.hComp_iso_inv_app,
-    whiskerRight_app, Functor.rightUnitor_inv_app, comp_id, assoc]
+  simp [η]
 
 end Localization
 
